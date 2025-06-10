76d4e7ad7e147ff754cd11f079966cc97f715b31
nataliadybczak <natalka.dybczak@gmail.com>
2025-06-10 17:48:02 +0200

    sidebar improvements

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,54 +2,30 @@ import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Settings, Users, Building2 } from "lucide-react"
-
-const sidebarItems = [
-  {
-    title: "Actions",
-    icon: Settings,
-    href: "/actions",
-  },
-  {
-    title: "Board",
-    icon: Users,
-    href: "/board",
-  },
-  {
-    title: "Companies",
-    icon: Building2,
-    href: "/companies",
-  },
-];
+import { SIDEBAR_ITEMS } from "./sidebar.const";
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
             <SidebarMenu>
-          {sidebarItems.map((item) => (
+          {SIDEBAR_ITEMS.map((item) => (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild>
-                <a href={item.href}>
+                <a href={item.href} className="flex items-center ml-2 text-inherit text-base no-underline">
                   <item.icon />
-                  <span>{item.title}</span>
+                  <span className="font-medium text-base ml-2">{item.title}</span>
                 </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
             </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
